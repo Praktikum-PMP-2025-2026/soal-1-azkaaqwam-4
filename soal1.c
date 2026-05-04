33bf8bf99da3efd78ff40dee2f0233e4f22c62ba
azkaaqwam <azkamhd08@gmail.com>
2026-05-04 09:17:53 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,128 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 4 - Dynamic Structures
+ *   Hari dan Tanggal    : Senin,4 Mei 2026
+ *   Nama (NIM)          : Azka Muhammad Aqwam(13224061)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Menggunakan dynamic structures yang betujuan untuk mengetahui posisinya dan list nya juga
+ * 
+ */
 
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* next;
+} node;
+
+node* membuatnode(int nilai) {
+    node* nodebaru = (node*)malloc(sizeof(node));
+    nodebaru->data = nilai;
+    nodebaru->next = NULL;
+    return nodebaru;
+}
+
+void pushfront(node** head, int nilai) {
+    node* nodebaru = membuatnode(nilai);
+    nodebaru->next = *head;
+    *head = nodebaru;
+}
+
+void pushback(node** head, int nilai) {
+    node* nodebaru = membuatnode(nilai);
+    if (*head == NULL) {
+        *head = nodebaru;
+        return;
+    }
+    node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = nodebaru;
+}
+
+void delete(node** head, int nilai) {
+    if (*head == NULL) return;
+
+    node* temp = *head;
+    node* prev = NULL;
+
+    if (temp != NULL && temp->data == nilai) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != nilai) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) return;
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+void find(node* head, int nilai) {
+    int index = 0;
+    node* temp = head;
+    while (temp != NULL) {
+        if (temp->data == nilai) {
+            printf("FOUND %d\n", index);
+            return;
+        }
+        temp = temp->next;
+        index++;
+    }
+    printf("NOT FOUND\n");
+}
+
+// Fungsi untuk mencetak seluruh list
+void print(node* head) {
+    if (head == NULL) {
+        printf("LIST EMPTY\n");
+        return;
+    }
+    printf("LIST ");
+    node* temp = head;
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        if (temp->next != NULL) printf(" ");
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int q;
+    if (scanf("%d", &q) != 1) return 0;
+
+    node* head = NULL;
+
+    for (int i = 0; i < q; i++) {
+        int op, x;
+        scanf("%d %d", &op, &x);
+
+        if (op == 1) {
+            pushfront(&head, x);
+        } else if (op == 2) {
+            pushback(&head, x);
+        } else if (op == 3) {
+            delete(&head, x);
+        } else if (op == 4) {
+            find(head, x);
+        }
+    }
+
+    print(head);
+
+    node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
     return 0;
 }
